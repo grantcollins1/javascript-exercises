b3d4146b4ce65f46c1d31efa66f3b1a901201e5f
gcollins <grant.collins@codeware.com>
2025-07-23 14:12:19 +0000

    add array work

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(intOne, intTwo) {
+  let larger = intOne;
+  let smaller = intTwo;
+  if (!Number.isInteger(intOne) || !Number.isInteger(intTwo) || intTwo < 0 || intOne < 0) {
+    return "ERROR";
+  }
+  let total = 0;
+  if (intTwo > intOne) {
+    larger = intTwo;
+    smaller = intOne;
+  }
+  for (let i = smaller; i <= larger; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
