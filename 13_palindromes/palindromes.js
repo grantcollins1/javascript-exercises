c403d0066f2304de6df1496f7cb935c69128b0b8
gcollins <grant.collins@codeware.com>
2025-07-24 20:57:35 +0000

    complete 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const stringArray = string.split('');
+  let cleanStringArray = stringArray.map((char) => char.toLowerCase());
+  cleanStringArray = cleanStringArray.filter((char) => alphanumerical.includes(char))
+  console.log(cleanStringArray);
+  let normalArray = [...cleanStringArray];
+  cleanStringArray.reverse();
+  const reversal = cleanStringArray.join('');
+  const normal = normalArray.join('');
+  return normal === reversal;
 };
 
 // Do not edit below this line
