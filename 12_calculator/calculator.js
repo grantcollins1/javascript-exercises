1ad851af75c1e307b726c437b0f621d93451aa9d
gcollins <grant.collins@codeware.com>
2025-07-24 20:26:09 +0000

    complete 12 calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+ num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, curItem) => total + curItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, curItem) => total * curItem);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let total = num;
+	for (let i = 1; i < power; i++) {
+    total *= num;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let curNum = num;
+  let total = 1;
+  while (curNum > 1) {
+    total *= curNum;
+    curNum -= 1;
+  }
+  return total;
 };
 
 // Do not edit below this line
