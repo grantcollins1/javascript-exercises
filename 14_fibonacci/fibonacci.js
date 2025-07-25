f18edceb61ffe259004873d655010f7baf3368eb
gcollins <grant.collins@codeware.com>
2025-07-25 13:55:51 +0000

    complete fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  let arr = [0, 1];
+  num = Number(num);
+  if (num < 0) {
+    return "OOPS";
+  }
+  for (i = 0; i < num; i++) {
+    arr.push(arr[i] + arr[i+1]);
+  }
+  return arr[num];
 };
 
 // Do not edit below this line
