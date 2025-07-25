d7ae6a521d892c688944155a217c2f5d669564f6
gcollins <grant.collins@codeware.com>
2025-07-25 14:10:26 +0000

    complete find the oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let person;
+  let age = 0;
+  people.forEach((p) => {
+    const death = p.yearOfDeath ? p.yearOfDeath : 2025;
+    if (death - p.yearOfBirth > age) {
+      age = death - p.yearOfBirth;
+      person = p;
+    }
+  })
+  return person;
 };
 
 // Do not edit below this line
